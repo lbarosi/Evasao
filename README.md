b2fb66907ed6638d29e68be1e1bc13fd5d371f92
Luciano Barosi <lbarosi@gmail.com>
2017-02-27 21:54:56 -0300

    DADOS

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Evasao
+# **Estudo de Indicadores de Evasao**
+
+
+## Objetivo
+
+## Hipóteses Principais
+
+## Investigações Preliminares
+
+## Rscripts
+
+## Conteúdo em Dados
+
+### Alunos
+
+Arquivo CSV com o vínculo de alunos de 2002.1 a 2016.1, incluindo forma de ingresso, forma de evasão. 
+
+#### **Descrição das variáveis**
+
+Nome | Descrição
+-----|----------
+MATRÍCULA | TESTE
+
+### Rendimento
+
+Arquivos por centro e por período com as informações de desempenho acadêmicas de todos os alunos ativos no período indicado.
+
+#### **Descrição das variáveis**
---
+++ Rscripts/digest-tables.R
@@ -0,0 +1,53 @@
+#!/usr/bin/Rscript
+options(java.parameters = "-Xmx6096m")
+library(readxl)
+library(digest)
+library(xlsx)
+library(magrittr)
+###########
+#Hash de Tabelas
+
+#Alunos
+
+Alunos <- read.csv("../../DADOS-PESSOAIS/DADOS/Alunos/Alunos.csv", sep = ",")
+
+
+Alunos$COD_INGRESSO %<>% as.numeric()
+Alunos$COD_EVASAO %<>% as.numeric()
+#------------------
+Alunos$CPF <- sapply(Alunos$CPF, digest, algo = "md5")
+Alunos$MATRÍCULA <- sapply(Alunos$MATRÍCULA, digest, algo = "md5")
+
+write.csv(as.data.frame(Alunos), "../DADOS/Alunos/Alunos.csv", row.names = FALSE)
+
+cat("\n HASH de Alunos disponível em ../DADOS/Alunos/Alunos.csv \n")
+
+
+#Desempenho
+
+files       <- list.files("../../DADOS-PESSOAIS/DADOS/Desempenho","ChCRAAnos")
+
+files       <- sapply(files, function(x) {
+  paste("../../DADOS-PESSOAIS/DADOS/Desempenho/",x, sep = "" )
+    }
+  ) 
+n <- length(files)
+
+for (i in c(1:n)){
+  if(file.size(files[[i]]) != 0){
+    getfiles <- read.csv(files[[i]], skip = 1)
+    getfiles$Aluno <- sapply(getfiles$Aluno, digest, algo = "md5")
+    nome <- substr(files[[i]],39,nchar(files[[i]]))
+    nome <- paste("../DADOS/Rendimento/",nome, sep = "")
+    nome %<>% substr(.,1,nchar(.)-4) %>% paste(.,"csv",sep="")
+    
+    write.csv(as.data.frame(getfiles), nome, row.names = FALSE)
+    cat(paste("\n", i,"/",n," HASH disponível: ", nome, "\n", sep = ""))
+    rm(getfiles)}
+}
+
+######################################################################
+
+
+
+
